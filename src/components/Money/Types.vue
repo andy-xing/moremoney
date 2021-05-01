79ebd5c657c47d17343023ffa864dd8c09fa12e4
andy.xing <991376602@qq.com>
2021-05-02 06:21:39 +0800

    使用TS实现了type组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,29 +1,35 @@
 <template>
   <div>
-    <ul class="types">
+    <ul class="types" >
       <li :class="type === '-' && 'selected' "
-     @click="selectType('-')" >支出</li>
+          @click="selectType('-')">支出
+      </li>
       <li :class="type === '+' && 'selected'"
-      @click="selectType('+')" >收入</li>
+          @click="selectType('+')">收入
+      </li>
     </ul>
   </div>
 </template>
 
-<script lang="js">
-export default {
-  name: "Types",
-  data(){
-    return{
-      type:'-' //'-'表示支出，'+'表示收入
+<script lang="ts">
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+@Component
+export default class Types extends Vue {
+  type = '-'; //'-'表示支出，'+'表示收入
+
+  @Prop(Number) readonly xxxx: number | undefined;
+  //Props 告诉 vue xxxx不是data 是prop
+  //Number 告诉 vue xxxx 运行时是number
+  //xxxx是属性名
+  //number | undefined告诉TS xxxx编译时的类型
+
+  selectType(type: string) {
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
     }
-  },
-  methods:{
-    selectType(type){
-      if (type !== '-'&& type !== '+'){
-        throw new Error('type is unknown')
-      }
-      this.type = type
-    }
+    this.type = type;
   }
 }
 </script>
@@ -34,22 +40,24 @@ export default {
   display: flex;
   text-align: center;
   font-size: 24px;
-> li {
-  width: 50%;
-  height: 64px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-&.selected::after {
-   content: '';
-   position: absolute;
-   bottom: 0;
-   left: 0;
-   width: 100%;
-   height: 4px;
-   background: #333;
- }
-}
+
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &.selected::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
 }
 </style>
